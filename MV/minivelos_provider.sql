24daf79dec85d3605427c792061f6f49f036f8ed
Inigo San Gil <inigo.sangil@gmail.com>
2022-03-18 11:43:42 -0600

    provider restrict set to unm docs and pis

--- MV/minivelos_provider.sql
+++ MV/minivelos_provider.sql
@@ -5,6 +5,7 @@
 -- Then will need to add provider to mq_observation_study (but there is no field for it)...Has RS21 given feedback on other candidtates for storing patient/study/PI data?
 SELECT 'IDENTITY_CONTEXT','SOURCE_PK','PROVIDER_ID','PROVIDER_NAME',
 'NPI','DEA','SPECIALTY_CONCEPT_ID','CARE_SITE_ID','YEAR_OF_BIRTH','GENDER_CONCEPT_ID','PROVIDER_SOURCE_VALUE','SPECIALTY_SOURCE_VALUE','SPECIALTY_SOURCE_CONCEPT_ID','GENDER_SOURCE_VALUE','GENDER_SOURCE_CONCEPT_ID','modified_dtTm'
+
 UNION ALL
 SELECT DISTINCT
 	'MINIVELOS DM_PATIENT_ENROLLMENTS (OMOP_PROVIDER)' AS IDENTITIY_CONTEXT
@@ -25,19 +26,16 @@ SELECT DISTINCT
         ,DATE_FORMAT(curDate(),'%Y-%m-%d %H:%i:%s') As modified_DtTm 
 FROM MINIVELOS.DM_PATIENT_ENROLLMENTS  src
 INNER JOIN MINIVELOS.dm_Study st on src.pkStudy_st = st.pkStudy_st
-LEFT JOIN MINIVELOS.ER_USER usr on st.piLast_st_lu = usr.usr_lastName and st.piFirst_st_lu = usr.usr_firstName
-WHERE src.PkStudy_St is not null and src.PersonCode_P is not null
+INNER JOIN MINIVELOS.ER_USER usr on st.piLast_st_lu = usr.usr_lastName and st.piFirst_st_lu = usr.usr_firstName
+WHERE src.PkStudy_St is not null 
+and src.PersonCode_P is not null
 	and src.Enroll_dt_pp >= '2010-01-01'
 and  (		
 		src.treatmentOrg_pp_lu ='UNM - CRTC'   
     or (src.treatmentOrg_pp_lu is null and src.enrollOrg_pp_lu = 'UNM - CRTC')
     )   
 and src.PERSONCODE_P <> '00001234 TestPatient'
-and usr.pk_user > 0
--- WHERE FK_siteID = 50 -- 'UNM - CTRC'
-
-
-
+and usr.pk_user > 0 
 INTO OUTFILE 'D:\\KRIIS_ETLs\\Sources\\mv\\minivelos_provider.dat'
 FIELDS TERMINATED BY '|'
 ESCAPED BY "" 
