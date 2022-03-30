b09a3c02d8b146c1658bd928243b11eabc43ab1e
Inigo San Gil <inigo.sangil@gmail.com>
2022-03-30 15:34:37 -0600

    debbie adds pkstudy, cleans study-title & DISTINCT

--- MV/minivelos_observation_period.sql
+++ MV/minivelos_observation_period.sql
@@ -19,13 +19,16 @@
     -- Tried this but it timed out; ,(select statusDt_pss  From Minivelos.DM_Patient where DM_Patient.PersonCode_p = src.PersonCode_P and DM_Patient.studyNumber_st = src.studyNumber_st order by statusDt_pss desc Limit 1) AS OBSERVATION_PERIOD_END_DATE      
 -- contraint for UNMCCC patients: need to confirm that this logic identifies UNM Health System patients; 
 	-- if it does, note that patients may NOT have been seen at UNMCCC (ex: May have been UH or Pediatric Patients) 
- -- REMOVED FIELD BECAUSE NOT IN OMOP DD:  STUDYNUMBER_ST AS OBSERVATION_PERIOD_VALUE        
-SELECT 'IDENTITY_CONTEXT','SOURCE_PK'	,'OBSERVATION_PERIOD_ID','PERSON_ID','OBSERVATION_PERIOD_START_DATE','OBSERVATION_PERIOD_END_DATE','PERIOD_TYPE_CONCEPT_ID','modified_DtTm' 
+ -- REMOVED FIELD BECAUSE NOT IN OMOP DD:  STUDYNUMBER_ST AS OBSERVATION_PERIOD_VALUE   
+-- 03/30/2022 Added Distinct (source DM has multiple rows due to data we are not collecting here) DAH
+-- 03/30/2022 Added code to remove carriage returns and '|' from title  DAH
+-- 3/30/2022 added -- ,src.PKSTUDY_ST as Study_PK    
+SELECT 'IDENTITY_CONTEXT','SOURCE_PK'	,'OBSERVATION_PERIOD_ID','PERSON_ID','OBSERVATION_PERIOD_START_DATE','OBSERVATION_PERIOD_END_DATE','PERIOD_TYPE_CONCEPT_ID','Study_PK','modified_DtTm' 
 UNION ALL
-SELECT 
+SELECT DISTINCT
    'MINIVELOS DM_PATIENT_ENROLLMENTS(OMOP_OBSERVATION_PERIOD)' AS IDENTITY_CONTEXT
    ,concat_ws("-",src.PKSTUDY_ST,src.PERSONCODE_P)  AS SOURCE_PK    -- Correct PK, but we need specific to record. Concat PKStudy_St,PersonCode_p  -- should be unique								
-   ,src.TITLE_ST     AS OBSERVATION_PERIOD_ID 						 
+   ,RTRIM(REPLACE(REPLACE(REPLACE(src.TITLE_ST, CHAR(13), ''), CHAR(10), ''), '|','-' ))      AS OBSERVATION_PERIOD_ID 						 
    ,src.PERSONCODE_P AS PERSON_ID      -- MRN                       
    ,DATE_FORMAT(src.ENROLL_DT_PP,'%Y-%m-%d %H:%i:%s')  AS OBSERVATION_PERIOD_START_DATE    		
    ,CASE						
@@ -34,6 +37,7 @@ SELECT
 	ELSE ''
    END OBSERVATION_PERIOD_END_DATE 
    ,'surrogate for CTMS EVELOS' AS PERIOD_TYPE_CONCEPT_ID -- Questionable, but consistent with other extracts
+   ,src.PKSTUDY_ST as Study_PK    -- added 3/30/2022
    ,DATE_FORMAT(curDate(),'%Y-%m-%d %H:%i:%s') As modified_DtTm  
 FROM MINIVELOS.DM_PATIENT_ENROLLMENTS  src
 WHERE src.PkStudy_St is not null and src.PersonCode_P is not null
